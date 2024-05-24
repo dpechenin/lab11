37c21506505df99638991826907af6ac456206d2
Данила Печенин <dpechenin@icloud.com>
2024-05-24 22:36:58 +0300

    binary tree is working

---
+++ lab11/Header.h
@@ -0,0 +1,12 @@
+//
+//  Header.h
+//  lab11
+//
+//  Created by Данила Печенин on 24.05.2024.
+//
+
+#ifndef Header_h
+#define Header_h
+
+
+#endif /* Header_h */
---
+++ lab11/tree.c
@@ -0,0 +1,8 @@
+//
+//  tree.c
+//  lab11
+//
+//  Created by Данила Печенин on 24.05.2024.
+//
+
+#include <stdio.h>
